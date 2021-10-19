70300f8e0f98759a6f783aabf302e5be08aef0a7
Ezra Lazuardy <ezralucio@gmail.com>
2021-10-19 19:31:34 +0700

    Minor Bug Fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,18 +8,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        if ($this->app->isLocal() || config('app.env_ci_cd')) {
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
@@ -27,5 +15,55 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        // register & set the configuration only on production environment
+        if (app()->isProduction()) {
+
+            // make sure to use secure scheme in production environment
+            URL::forceScheme('https');
+            $this->app['request']->server->set('https', true);
+        }
+
+        // register & set the configuration only on local environment
+        if ($this->app->isLocal() || config('app.env_ci_cd')) {
+
+            // register the IDE Helper for local environment
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
+
+        // register the application macros
+        $this->registerMacros();
+    }
+
+    /**
+     * Register any application macros.
+     *
+     * @return void
+     */
+    private function registerMacros(): void
+    {
+        if (!Collection::hasMacro('paginate')) {
+            Collection::macro('paginate', function ($perPage = 15, $total = null, $page = null, $pageName = 'page') {
+                $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+                return new LengthAwarePaginator(
+                    $this->forPage($page, $perPage),
+                    $total ?: $this->count(),
+                    $perPage,
+                    $page,
+                    [
+                        'path' => LengthAwarePaginator::resolveCurrentPath(),
+                        'pageName' => $pageName,
+                    ]
+                );
+            });
+        }
     }
 }
