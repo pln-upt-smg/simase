cf2934de88360d7b5150ffadc3a2b779cb6fabe9
Ezra Lazuardy <ezralucio@gmail.com>
2021-10-19 22:32:23 +0700

    Minor Bug Fixes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,10 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+};
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
