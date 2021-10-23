831b4a2d32116d70f36d543b417ed67de10884f6
Ezra Lazuardy <ezralucio@gmail.com>
2021-10-23 19:43:13 +0700

    Add Laravel Cloudflare

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,12 +19,12 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('cloudflare:reload')->daily();
     }
 
     /**
@@ -32,10 +32,9 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
-
+        $this->load(__DIR__ . '/Commands');
         require base_path('routes/console.php');
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Stocktake">
+    <meta name="description" content="📦 Item Stock Management System for Unilever Indonesia.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Stocktake') }}</title>
     <link rel="preconnect" href="{{ config('app.url') }}">
