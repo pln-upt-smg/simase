39da59d1304b8df6ecf66d07fff87399bf81c940
Ezra Lazuardy <ezralucio@gmail.com>
2021-11-19 11:13:50 +0700

    Minor Bug Fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('cloudflare:reload')->daily();
+        $schedule->command('cloudflare:reload')->daily()->withoutOverlapping();
     }
 
     /**
