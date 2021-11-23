267a122eb43a7315a46428f15aeeb7370579544f
Ezra Lazuardy <ezralucio@gmail.com>
2021-11-23 21:44:24 +0700

    Minor Bug Fixes

--- app/Exports/BatchesExport.php
+++ app/Exports/BatchesExport.php
@@ -2,8 +2,6 @@
 
 namespace App\Exports;
 
-use App\Notifications\DataExported;
-use App\Services\BatchService;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -12,35 +10,33 @@ use Maatwebsite\Excel\Concerns\WithMapping;
 
 class BatchesExport implements FromCollection, WithHeadings, WithMapping
 {
-    private BatchService $batchService;
+	private Collection $collection;
 
-    public function __construct(BatchService $batchService)
-    {
-        $this->batchService = $batchService;
-    }
+	public function __construct(Collection $collection)
+	{
+		$this->collection = $collection;
+	}
 
-    public function headings(): array
-    {
-        return [
-            'Batch',
-            'Material',
-            'SLoc'
-        ];
-    }
+	public function headings(): array
+	{
+		return [
+			'Batch',
+			'Material',
+			'SLoc'
+		];
+	}
 
-    public function map($row): array
-    {
-        return [
-            Str::upper(trim($row->code)),
-            Str::upper(trim($row->material_code)),
-            trim($row->sloc)
-        ];
-    }
+	public function map($row): array
+	{
+		return [
+			Str::upper(trim($row->code)),
+			Str::upper(trim($row->material_code)),
+			trim($row->sloc)
+		];
+	}
 
-    public function collection(): Collection
-    {
-        $data = $this->batchService->collection();
-        auth()->user()?->notify(new DataExported('Batch', $data->count()));
-        return $data;
-    }
+	public function collection(): Collection
+	{
+		return $this->collection;
+	}
 }
